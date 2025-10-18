ee1b6e14ab877284f452e485b0ab144b0b1493d1
C Rohit <125362649+Rohit-2710@users.noreply.github.com>
2025-10-18 18:13:31 +0530

    Time: 0 ms (100%), Space: 41.3 MB (46.06%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -10,23 +10,36 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        int n=1;
+        // int n=1;
+        // ListNode node = new ListNode();
+        // node=head;
+        // while(node.next!=null){
+        //     n+=1;
+        //     node=node.next;
+        // }
+        // int m =0;
+        // m=n/2;
+        // int j=0;
+        // node=head;
+        // while(j<m){
+        //  node=node.next;
+        //  j++;
+        // }
+
+        // return node;
+        List<ListNode> a = new ArrayList<>();
         ListNode node = new ListNode();
         node=head;
+        a.add(node);
         while(node.next!=null){
-            n+=1;
             node=node.next;
-        }
-        int m =0;
-        m=n/2;
-        int j=0;
-        node=head;
-        while(j<m){
-         node=node.next;
-         j++;
+            a.add(node);
         }
 
-        return node;
+        int l = a.size();
+        int m=l/2;
+
+        return a.get(m);
         
     }
 }
