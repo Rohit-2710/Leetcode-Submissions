b6979a0300ba05df1c5c66184d196bfc894313a8
C Rohit <125362649+Rohit-2710@users.noreply.github.com>
2025-10-18 19:23:22 +0530

    Time: 0 ms (100%), Space: 43.2 MB (6.02%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -14,17 +14,35 @@ class Solution {
         if(head == null){
             return head;
         }
-       
-        ListNode node = head;
-        ListNode temp = new ListNode(head.val);
-
-        while(node.next!=null){
-            node=node.next;
-            ListNode newNode = new ListNode(node.val);
-            newNode.next=temp;
-            temp=newNode;
+        if(head.next==null){
+            return head;
         }
-        return temp;
+       
+        // ListNode node = head;
+        // ListNode temp = new ListNode(head.val);
+
+        // while(node.next!=null){
+        //     node=node.next;
+        //     ListNode newNode = new ListNode(node.val);
+        //     newNode.next=temp;
+        //     temp=newNode;
+        // }
+        // return temp;
+
+        class rec{
+            public ListNode list(ListNode node,ListNode temp){
+                ListNode newNode= new ListNode(node.val);
+                newNode.next = temp;
+                if(node.next==null){
+                    return newNode;
+                }
+                return list(node.next,newNode);
+            }
+        }
+
+        rec obj = new rec();
+        ListNode temp = new ListNode(head.val);
+        return obj.list(head.next,temp);
         
     }
 }
