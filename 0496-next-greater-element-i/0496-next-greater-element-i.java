242241fc4342411f01caeb34a0d7e4a80920cd78
C Rohit <125362649+Rohit-2710@users.noreply.github.com>
2025-11-04 19:57:58 +0530

    Time: 3 ms (87.5%), Space: 45.5 MB (7.12%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -5,38 +5,63 @@ class Solution {
         int n2=nums2.length;
 
         int[] ans = new int[n1];
+        int[] h = new int[10001];
 
-        Arrays.fill(ans,-1);
+        Arrays.fill(h,-1);
 
-        for(int i=n1-1;i>=0;i--){
-            int val = nums1[i];
-            int j = n2-1;
+        // for(int i=n1-1;i>=0;i--){
+        //     int val = nums1[i];
+        //     int j = n2-1;
 
-            Stack<Integer> st = new Stack<>();
+        //     Stack<Integer> st = new Stack<>();
 
-            while(val!=nums2[j]){
-                if(st.empty()){
-                    st.push(nums2[j]);
-                    j--;
-                    continue;
-                }
-                while(!st.empty()&&st.peek()<=nums2[j]){
-                    st.pop();
-                }
-                st.push(nums2[j]);
-                j--;
-            }
+        //     while(val!=nums2[j]){
+        //         if(st.empty()){
+        //             st.push(nums2[j]);
+        //             j--;
+        //             continue;
+        //         }
+        //         while(!st.empty()&&st.peek()<=nums2[j]){
+        //             st.pop();
+        //         }
+        //         st.push(nums2[j]);
+        //         j--;
+        //     }
+        //     while(!st.empty()&&st.peek()<=val){
+        //             st.pop();
+        //     }
+        //     if(!st.empty()&&st.peek()>val){
+        //         ans[i]=st.peek();
+        //     }
+
+
+        // }
+        // return ans;
+
+        Stack<Integer> st = new Stack<>();
+
+        int i = n2-1;
+
+        while(i>=0){
+            int val =nums2[i];
             while(!st.empty()&&st.peek()<=val){
-                    st.pop();
+                st.pop();
             }
-            if(!st.empty()&&st.peek()>val){
-                ans[i]=st.peek();
+            if(!st.empty()){
+                h[val]=st.peek();
             }
+            st.push(val);
+            i--;
+        }
 
-
+        for(int j=0;j<n1;j++){
+            int r = nums1[j];
+            ans[j]=h[r];
         }
 
         return ans;
+
+
 
         
         
