d067d7b204f1f6598af5ec866c23aa24fac82f37
C Rohit <125362649+Rohit-2710@users.noreply.github.com>
2025-11-14 22:51:25 +0530

    Time: 12 ms (4.8%), Space: 67.6 MB (5.02%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        Map<Integer,Integer> valueIndexMap=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            valueIndexMap.put(i,nums[i]);
+        }
+        for(Map.Entry<Integer, Integer> entry : valueIndexMap.entrySet()){
+            int key=entry.getKey();
+            int value=entry.getValue();
+            int newIndex=(key+k)%nums.length;
+            nums[newIndex]=value;
+        }
+        
+    }
+}
+
